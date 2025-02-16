50de7280c0f0c72e0de4a5bcd245e3b1aa1221b0
Quantiluzarra <156359929+Quantiluzarra@users.noreply.github.com>
2025-02-16 15:52:26 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+# Токен вашего Telegram бота (получите у @BotFather)
+TOKEN = '7881659124:AAH61KoBUM5PZlcf3BY5BaQAcI3FS0dpHKk'
+
+# Поддерживаемые языки
+LANGUAGES = ['ru', 'en']
+
+# Уровни сложности
+DIFFICULTIES = {
+    'Легкая': 'easy',
+    'Средняя': 'medium',
+    'Сложная': 'hard'
+}
